208b064e6adde1b2e2a7a274cd045651091b6ff1
Andri11i <zotov.andrey.2008@gmail.com>
2024-10-31 15:00:57 +0200

    Add project files.

---
+++ Calculator_HW/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_HW
+{
+    public class Calculator
+    {
+        public double Add(double a, double b) => a + b;
+        public double Subtract(double a, double b) => a - b;
+        public double Multiply(double a, double b) => a * b;
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Cannot divide by zero.");
+            return a / b;
+        }
+
+        public double Average(double[] numbers)
+        {
+            if (numbers.Length == 0)
+                throw new ArgumentException("Array cannot be empty.");
+            return numbers.Average();
+        }
+
+        public double Max(double[] numbers)
+        {
+            if (numbers.Length == 0)
+                throw new ArgumentException("Array cannot be empty.");
+            return numbers.Max();
+        }
+
+        public double[] Sort(double[] numbers) => numbers.OrderBy(n => n).ToArray();
+    }
+
+}
---
+++ Calculator_HW/CalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+
+namespace Calculator_HW
+{
+    public class CalculatorTests
+    {
+        private readonly Calculator _calculator;
+
+        public CalculatorTests()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Fact]
+        public void Add_ReturnsCorrectSum()
+        {
+            Assert.Equal(5, _calculator.Add(2, 3));
+        }
+
+        [Fact]
+        public void Subtract_ReturnsCorrectDifference()
+        {
+            Assert.Equal(1, _calculator.Subtract(3, 2));
+        }
+    }
+}
+
